a7f3b60a6217e7c83556bf9f8274b190d58e8f43
TzolkinB <krbell4@gmail.com>
2018-08-07 18:41:39 -0400

    style edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -17,43 +17,55 @@ const Home = () => {
         </h3>
       </div>
       <div id="about" className="container-fluid"> 
-        <div className="row section-padding">
-          <div className="col-sm-12 mb-3">
-            <h2>About Me</h2>
+        <div className="section-padding">
+          <div className="row">
+            <div className="col-sm-12 mb-3">
+              <h2>About Me</h2>
+            </div>
           </div>
           <div className="row">
-            <div className="col-sm-7 ml-4">
-                <img src={Me} alt="Profile image" height="120" width="120" className="rounded-circle float-left mr-3"/>
-                <p className="pl-2">I am a front-end developer and I love using ES6 and the React ecosystem.
-                In my current job, I often convert html files into react components, including implementing components from the chosen
-                css framework. I also add javascript to the initial prototype applications intended for client approval.
-                Additional tasks include updating existing code with structure and style changes as nessessary. In
-                my free time I continue to deepen my understanding of Javascript and learn Node. 
-                When I am not coding I love to read and be outside enjoying my backyard!</p>
-                <p>You might be wondering, why "Fierce Whiskers"? It is a very simple answer. I have three cats.
-                One we refer to as "fierce whiskers", one referred to as "chubbers", and the other we actually
-                named "Scragalea". Chubbers and Scragalea did not sound like great choices for domain name and
-                anything I came up with using my name was already taken.</p>
-                <p>I am always looking for new opportunites so feel free to reach out to me: krbell4@gmail.com</p>
+            <div className="col-sm-12">
+              <img src={Me} alt="Profile image" height="120" width="120" className="rounded-circle float-left mr-4"/>
+              <p className="pl-2">I am a front-end developer and I love using ES6 and the React ecosystem.
+              In my current job, I often convert html files into react components, including implementing components from the chosen
+              css framework. I also add javascript to the initial prototype applications intended for client approval.
+              Additional tasks include updating existing code with structure and style changes as nessessary. In
+              my free time I continue to deepen my understanding of Javascript and learn Node. 
+              When I am not coding I love to read and be outside enjoying my backyard!</p>
+              <p>I am always looking for new opportunites so feel free to reach out to me: krbell4@gmail.com</p>
             </div>
-            <div className="col-sm-4">
-              <h3>Skills</h3>
-              <ul className="skills-col">
+          </div>
+        </div>
+      </div>
+      <div id="skills" className="container-fluid"> 
+        <div className="section-padding">
+          <div className="row">
+            <div className="col-sm-12 mb-3">
+              <h2 className="text-center">Skills</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-2">
+            </div>
+            <div className="col-sm-8">
+              <ul className="skills-col mb-0">
                 <li>HTML</li>
                 <li>CSS</li>
                 <li>Javascript/ES6</li>
                 <li>React</li>
                 <li>Redux</li>
                 <li>Webpack</li>
-                <li>Github</li>
-                <li>VIM</li>
-                <li>Bootstrap</li>
-                <li>Bootstrap Material Design</li>
+                <li>Bootstrap/ Bootstrap Material Design</li>
                 <li>Zurb Foundation</li>
                 <li>Materialize</li>
+                <li>Github</li>
+                <li>VIM</li>
                 <li>Sketch</li>
-                <li>Adobe Suite</li>
+                <li>Adobe XD</li>
+                <li>Adobe Pro DC</li>
               </ul>
+            </div>
+            <div className="col-sm-2">
             </div>
           </div>
         </div>
@@ -70,7 +82,7 @@ const Home = () => {
               <h5 className="card-title">Hazmat</h5>
               <h6 className="card-subtitle mb-2 text-muted">Material Design Library</h6>
               <p className="card-text">Library of React components based on Material Design guidelines for use by TMA.</p>
-              <a href="https://qa-tma1.works/hazmat/" className="card-link">Demo</a>
+              <a href="https://qa-tma1.works/hazmat/" target="_blank" className="card-link">Demo</a>
             </div>
           </div>
           <div className="card card-width">
@@ -78,7 +90,7 @@ const Home = () => {
               <h5 className="card-title">React Starter Template</h5>
               <h6 className="card-subtitle mb-2 text-muted">Uses webpack</h6>
               <p className="card-text">Basic template for getting new applications up and running quickly.</p>
-              <a href="https://github.com/TzolkinB/react-template" className="card-link">Github Repo</a>
+              <a href="https://github.com/TzolkinB/react-template" target="_blank" className="card-link">Github Repo</a>
             </div>
           </div>
         </div>
--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -8,25 +8,21 @@ const Footer = () => {
 
   return (
     <footer className="container-fluid">
-      <div className="row">
-        <div className="col-sm-2 offset-sm-5">
-          <p className="text-white">&#169; {currentYear} Kimberly Bell</p>
-        </div>
-        <div className="col-sm-2 offset-sm-3">
-          <ul className="list-inline font-weight-light">
-            <li className="list-inline-item">
-              <a href="https://github.com/TzolkinB" target="_blank">
-                <img src={Github} alt="Github" width="40" height="40" />
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://www.linkedin.com/in/kimbell4" target="_blank">
-                <img src={LinkedIn} alt="LinkedIn"  width="40" height="40" />
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div className="copyright mt-2">
+        <p className="text-white">Copyright &#169; {currentYear} Kimberly Bell</p>
       </div>
+      <ul className="list-inline font-weight-light float-right mr-4">
+        <li className="list-inline-item px-2">
+          <a href="https://github.com/TzolkinB" target="_blank">
+            <img src={Github} alt="Github" width="40" height="40" />
+          </a>
+        </li>
+        <li className="list-inline-item px-2">
+          <a href="https://www.linkedin.com/in/kimbell4" target="_blank">
+            <img src={LinkedIn} alt="LinkedIn"  width="40" height="40" />
+          </a>
+        </li>
+      </ul>
     </footer>
   );
 }

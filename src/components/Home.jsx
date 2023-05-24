d92e1466ba310d602f2fc4d264b7a35df88a3a49
TzolkinB <krbell4@gmail.com>
2023-05-24 00:37:49 -0400

    update home jumbotron wip

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,23 @@ import Projects from './Projects';
 const Home = () => {
   return (
     <div className='container-fluid px-0'>
-      <div className='jumbotron jumbotron-fluid pt-5 mb-0' id='home'>
-        <div className='container text-center mt-5'>
+      {/* <div className='jumbotron jumbotron-fluid pt-5 mb-0' id='home'>
+        <div className='container text-end mt-5'>
           <h1 className='display-2 text-uppercase'>Kim Bell</h1>
           <h2 className='display-3'>SDET | Software Engineer in Test</h2>
           <h3>Cypress Test Automation. Previous Front End Developer.</h3>
         </div>
+      </div> */}
+      <div className="mask" id='home'>
+      <div className="d-flex justify-content-end align-items-center">
+        <div className="text-white">
+          <h1 className="mb-3 text-uppercase">Kim Bell</h1>
+          <h2 className="mb-3">SDET | Software Engineer in Test</h2>
+          <h3>Cypress Test Automation. Previous Front End Developer.</h3>
+          {/* <h1 className="mb-3">Heading</h1> */}
+          {/* <h4 className="mb-3">Subheading</h4> */}
+        </div>
+      </div>
       </div>
       <About id='about' />
       <Skills id='skills' />

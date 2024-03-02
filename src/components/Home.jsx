d3206231cd3c8b8789120520c19f00f437a2dbba
TzolkinB <krbell4@gmail.com>
2024-03-02 12:28:46 -0500

    completed navbar, footer, and most of home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
+import { MDBContainer, MDBTypography } from 'mdb-react-ui-kit';
 import About from './About';
 import Skills from './Skills';
 import Projects from './Projects';
 
 const Home = () => {
   return (
-    <div className='container-fluid px-0'>
-      <div id='home'>
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg" height={80}/>
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" height={80}/>
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg" height={80}/>
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" height={80}/>
-      <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" height={80}/>
-
-        <div className='container text-white pt-5'>
-          <h1 className='text-uppercase'>Kim Bell</h1>
-          <h2>SDET | Software Engineer in Test</h2>
-          <h3>Cypress Test Automation. Previous Front End Developer.</h3>
+    <MDBContainer fluid className="app px-0">
+      <div id="home" className="pe-3 d-flex flex-row-reverse justify-content-between align-items-center">
+        <div className="text-end">
+        <MDBTypography tag="h1" className="text-uppercase name">
+          Kim Bell
+        </MDBTypography>
+        <MDBTypography tag="h2" className="mb-0">
+          SDET | Software Engineer in Test
+        </MDBTypography>
+        <MDBTypography className="fs-4 mb-0">
+          Cypress Test Automation
+        </MDBTypography>
+        <MDBTypography className="fs-4 mb-0">
+          Previous Front End Developer
+        </MDBTypography>
+        </div>
+        <div>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cypressio/cypressio-original.svg" height={80}/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-plain.svg" height={80}/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/typescript/typescript-plain.svg" height={80}/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" height={80}/>
+        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" height={80}/>
         </div>
       </div>
       <About id='about' />
       <Skills id='skills' />
       <Projects id='projects' />
-    </div>
+    </MDBContainer>
   );
 };
 
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -12,10 +12,10 @@ const Footer = () => {
       </div>
       <div className="footer-links">
         <a href="https://github.com/TzolkinB" target="_blank" className="px-2 pt-4 pb-2">
-          <i class="devicon-github-original" style={{fontSize: 40}}></i>
+          <i className="devicon-github-original" style={{fontSize: 40}}></i>
         </a>
         <a href="https://www.linkedin.com/in/kimbell4" target="_blank" className="px-2 pt-4 pb-2">
-          <i class="devicon-linkedin-plain" style={{fontSize: 40}}></i>
+          <i className="devicon-linkedin-plain" style={{fontSize: 40}}></i>
         </a>
       </div>
       </MDBFooter>

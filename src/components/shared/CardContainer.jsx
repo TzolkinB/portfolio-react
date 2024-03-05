224a2ca18ff7601594844edf22144b001a6ab7cf
TzolkinB <krbell4@gmail.com>
2024-03-04 23:41:25 -0500

    cleanup

--- src/components/shared/CardContainer.jsx
+++ src/components/shared/CardContainer.jsx
@@ -4,7 +4,6 @@ import {
   MDBCardTitle,
   MDBCardSubTitle,
   MDBCardText,
-  MDBCardLink,
   MDBBtn
 } from 'mdb-react-ui-kit';
 
@@ -16,8 +15,6 @@ const CardContainer = props => {
 
   const multipleUrls = () => {
     if(url2) {
-      // return <a href={url2} target="_blank" className="card-link">{url2Text}</a>;
-      // return <MDBCardLink href={url2} target="_blank">{url2Text}</MDBCardLink>
       return <MDBBtn href={url2} target="_blank">{url2Text}</MDBBtn>
 
     }
@@ -25,21 +22,11 @@ const CardContainer = props => {
   }
 
   return (
-    // <div className="card card-width">
-    //   <div className="card-body">
-    //     <h5 className="card-title">{title}</h5>
-    //     <h6 className="card-subtitle mb-2 text-muted">{subtitle}</h6>
-    //     <p className="card-text">{text}</p>
-    //     <a href={url} target="_blank" className="card-link">{urlText}</a>
-    //     {multipleUrls()}
-    //   </div>
-    // </div>
     <MDBCard>
       <MDBCardBody>
         <MDBCardTitle>{title}</MDBCardTitle>
         <MDBCardSubTitle>{subtitle}</MDBCardSubTitle>
         <MDBCardText>{text}</MDBCardText>
-        {/* <MDBCardLink href={url} target="_blank">{urlText}</MDBCardLink> */}
         <MDBBtn href={url} target="_blank">{urlText}</MDBBtn>
         {multipleUrls()}
       </MDBCardBody>

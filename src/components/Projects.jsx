16d116dcb699b355990f1989b7d84b410c3cc236
TzolkinB <krbell4@gmail.com>
2024-03-04 23:39:43 -0500

    finish project responsive styling

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,10 +9,28 @@ const Projects = props => {
       <MDBTypography tag="h3" className="text-center">
         Projects
       </MDBTypography>
-        {/* <div className="d-flex justify-content-center"> */}
-      <MDBRow>
-        <MDBCol sm={3}></MDBCol>
-        <MDBCol sm={3}>
+      <div className="d-flex flex-column d-sm-none">
+        <Card
+            title="React Starter Template"
+            subtitle="Uses Webpack"
+            text="Basic template for getting new applications up and running quickly."
+            url="https://github.com/TzolkinB/react-template"
+            urlText="Github Repo"
+          />
+        <div className="py-2"></div>
+        <Card
+            title="Memory Game"
+            subtitle="Uses React, Webpack, Bootstrap Material Design"
+            text="Traditional memory card game for use by two players that declares a winner and can be replayed multiple times."
+            url="https://memory-game1234.firebaseapp.com/#/"
+            urlText="Demo"
+            url2="https://github.com/TzolkinB/memory"
+            url2Text="Github Repo"
+          />
+      </div>
+      <MDBRow className="d-none d-sm-flex">
+        <MDBCol size={1} lg={2} xl={3}></MDBCol>
+        <MDBCol size={5} lg={4} xl={3}>
           <Card
             title="React Starter Template"
             subtitle="Uses Webpack"
@@ -21,7 +39,7 @@ const Projects = props => {
             urlText="Github Repo"
           />
         </MDBCol>
-        <MDBCol sm={3}>
+        <MDBCol size={5} lg={4} xl={3}>
           <Card
             title="Memory Game"
             subtitle="Uses React, Webpack, Bootstrap Material Design"
@@ -32,9 +50,8 @@ const Projects = props => {
             url2Text="Github Repo"
           />
         </MDBCol>
-        <MDBCol sm={3}></MDBCol>
+        <MDBCol size={1} lg={2} xl={3}></MDBCol>
       </MDBRow>
-        {/* </div> */}
       </div>
   );
 }

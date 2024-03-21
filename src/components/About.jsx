660f7663614a8254397acec5b19b3bbf43da6e76
TzolkinB <krbell4@gmail.com>
2024-03-21 18:56:28 -0400

    fix about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,38 +1,54 @@
-import { MDBTypography, MDBListGroup, MDBListGroupItem, MDBBadge } from 'mdb-react-ui-kit';
-// import Me from 'IMG/profile.jpg';
+import { MDBTypography, MDBListGroup, MDBListGroupItem, MDBBadge, MDBIcon } from 'mdb-react-ui-kit';
 
 const About = props => {
   const { id } = props;
 
+  const accomplishments = [
+    "Created and maintained a 'Best Practices and Standards Guide' for Ally's implimentation of the framework and tools they were using.",
+    "Identified patterns used for common UI elements and added examples to ensure not only ease of adoption but also that all departments would meet the same quality and expectations.",
+    (<>Managed the global internal <span className="fw-bold">NPM</span> package, written in <span className="fw-bold">Javscript</span> and <span className="fw-bold">Typescript</span>, that contained global configurations in order to reduce the number of manual changes made by individual teams. This was used by 10 teams and multiple lines of business.</>),
+    (<>Added test jobs to <span className="fw-bold">Gitlab's CI/CD</span> pipeline using <span className="fw-bold">YAML</span> that allowed teams to trigger automated tests manually or on scheduled CICD pipeline runs, reducing the hardening time for release cycles from hours to minutes in many cases.</>),
+    (<>Developed a <span className="fw-bold">Bash</span> shell script to automate a needed change during Cypress version upgrade.</>),
+    (<>Developed and implemented <span className="fw-bold">Javascript</span> custom commands to increase efficiency writing tests.</>)
+  ]
+
   return (
     <div id={id} className="py-5 d-flex flex-column align-items-center">
       <div>
-      <MDBTypography tag='h3' className="text-white">
+      <MDBTypography tag='h3' className="text-white py-3">
         About Me
       </MDBTypography>
       </div>
-      <MDBTypography className="fw-light">
-        In 2022 I changed positions from a Senior Software Engineer working on Web UI to an SDET for automation testing. We used Cypress in conjunction with Testing-Library since it was already being used with Jest and was an easy way to add some accessibility coverage.
+      <MDBTypography className="fw-light align-self-start">
+        I have 7+ years experience in web devepment and 2+ years in test automation.
       </MDBTypography>
-      <ul className="fw-light" style={{ minWidth: '22rem' }}>
-      <li>implimenting and assisting in the transition from what was a one hundred percent manual process to a heavily automated testing process in the web space.</li>
-      <li>created and maintained a "Best Practices and Standards Guide" for Ally's implimentation of the framework and tools they were using. For example, I identified patterns used for common UI elements and added examples to ensure not only ease of adoption but also that all departments would meet the same quality and expectations.</li>
-      <li>worked on and helped maintain a global internal npm package that was used by 10 plus teams and multiple departments.</li>
-      <li> test jobs to Gitlab's CI/CD pipeline with YAML,</li>
-      <MDBBadge pill light color='primary'>
-          Onboarding
-        </MDBBadge>
-      <li> created a Bash script to add a required config for Cypress.</li>
-      <li>experience with Javascript and Typescript to write custom commands intended for team consumption in order to increaseefficiency in writing tests.</li>
-      </ul>
-         {/* My initial focus was on implimenting and assisting in the transition from what was a one hundred percent manual process to a heavily automated testing process in the web space. It was during this time that I created and maintained a "Best Practices and Standards Guide" for Ally's implimentation of the framework and tools they were using. For example, I identified patterns used for common UI elements and added examples to ensure not only ease of adoption but also that all departments would meet the same quality and expectations. 
-        In addition to training teams and individuals, my move to SDET has allowed me to gain a lot of experience in various other skills. I frequently worked on and helped maintain a global internal npm package that was used by 10 plus teams and multiple departments. This required knowledge of webpack as well as Javascript and Typescript. I added test jobs to Gitlab's CI/CD pipeline with YAML, and created a Bash script to add a required config for Cypress. I also used my experience with Javascript and Typescript to write custom commands intended for team consumption in order to increaseefficiency in writing tests.   */}
-      <div>
-        When I am not coding I love to read and be outside
-        
+      <MDBTypography className="fw-light">
+        In 2022 I changed positions from a Senior Software Engineer working on Web UI to an SDET for automation testing. I used <span className="fw-bold">Cypress</span> in conjunction with  <span className="fw-bold">Testing-Library</span>, which made queries incredibly human readable and added some accessibility coverage as well. In this role I:
+      </MDBTypography>
+      <MDBTypography listUnStyled className="mb-0 px-5" style={{ minWidth: '22rem' }}>
+        {accomplishments.map(accomplishment => {
+          console.log(accomplishment)
+          return (
+            <li className="mb-2 fw-light">
+              <MDBIcon icon='check-circle' className='me-2 text-success' />
+              {accomplishment}
+            </li> 
+          )
+        })
+      }
+      </MDBTypography>
+       {/* <MDBBadge pill light color='primary'>
+           Onboarding
+       </MDBBadge> */}
+         {/* My initial focus was on implimenting and assisting in the transition from what was a one hundred percent manual process to a heavily automated testing process in the web space. d
+        In addition to training teams and individuals, my move to SDET has allowed me to gain a lot of experience in various other skills.   */}
+      <MDBTypography className="fw-light align-self-start pt-5">
+        When I am not coding, I love to read and be outside. In fact, I often use lunch breaks as an opportunity to get away from the computer and take a walk. Since remote positions allow me to be closer to family, I also enjoy spending time with my nieces and nephews on the weekends now that I am able to live closer to them.
+      </MDBTypography>
+      <MDBTypography className="fw-light align-self-start pt-1">
         I am always looking for new opportunites so feel free to reach out
         to me: krbell4@gmail.com
-      </div>
+      </MDBTypography>
       {/* TODO: */}
           {/* Why remote only? */}
           {/* My Testing -- what is it from testing library? */}

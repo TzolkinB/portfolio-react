2d40c062aa1ed273a30c94769d59f925217c57e5
TzolkinB <krbell4@gmail.com>
2024-03-29 22:05:41 -0400

    wip testing skill section

--- cypress/e2e/profile.spec.cy.ts
+++ cypress/e2e/profile.spec.cy.ts
@@ -1,11 +1,11 @@
-import {
-  cloudHosting,
-  testingTools,
-  terminalTools,
-  webDevTools,
-} from "../../src/tech-icons"
+// import {
+//   cloudHosting,
+//   testingTools,
+//   terminalTools,
+//   webDevTools,
+// } from "../../src/tech-icons"
 
-describe("Home page tests", () => {
+describe("Profile tests", () => {
   beforeEach(() => {
     cy.intercept("localhost:4280").as("localhost")
     cy.visit("/")
@@ -47,7 +47,7 @@ describe("Home page tests", () => {
     )
   })
 
-  it.only("should have profile image in home section and bullet points in about me section", () => {
+  it("should have profile image in home section and bullet points in about me section", () => {
     cy.findByTestId("home").within(() => {
       cy.findByRole("heading", { level: 1, name: "Kim Bell" })
       cy.findByRole("heading", {
@@ -64,17 +64,47 @@ describe("Home page tests", () => {
   })
 
   it.only("should have tech icons and tooltips in skills section", () => {
+    const cloudHosting = ["gitlab", "bitbucket", "github", "vscode"]
+    const testingTools = ["cypress", "testing-library", "qtest", "browserstack"]
+    const terminalTools = ["git", "webpack", "yaml", "vim", "bash"]
+    const webDevTools = [
+      "javascript",
+      "typescript",
+      "react",
+      "ember",
+      "html",
+      "css",
+      "styled-components",
+    ]
     const allSkills = [
-      ...cloudHosting,
-      ...terminalTools,
       ...testingTools,
       ...webDevTools,
+      ...terminalTools,
+      ...cloudHosting,
     ]
 
     cy.findByTestId("skills").within(() => {
       cy.findByRole("heading", { level: 3, name: "Skills" })
 
-      cy.get("img").should("have.length", allSkills.length)
-    })
+      cy.get("img").should("have.length", allSkills.length).as("skillImages")
+
+      // eslint-disable-next-line prettier/prettier
+        // cy.get("@skillImages").each(($el) => {
+      // allSkills.forEach(skill => {
+          // cy.wrap($el).should("have.attr", "alt", skill)
+          // cy.get("img").should("have.attr", "alt", skill)
+        })
+    //   // })
+    // })
+
+    allSkills.map(skill => {
+      cy.get("@skillImages")
+          .each(($el) => {
+            cy.wrap($el).scrollIntoView()
+            .trigger("mouseover")
+            cy.findByRole("tooltip", { name: skill.toLocaleUpperCase() })
+            cy.wrap($el).trigger("mouseout")
+          })
+        })
   })
 })

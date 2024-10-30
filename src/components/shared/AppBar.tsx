388ca4414b3392204af34c601473ca156391d201
Kim Bell <krbell4@gmail.com>
2024-10-30 15:02:48 -0400

    update versions mdb-react-ui-kit, css-loader, cypress

--- src/components/shared/AppBar.tsx
+++ src/components/shared/AppBar.tsx
@@ -12,6 +12,7 @@ import {
   MDBDropdownToggle,
   MDBDropdownMenu,
   MDBDropdownItem,
+  MDBNavbarItem,
 } from "mdb-react-ui-kit"
 import devResume from "IMG/Bell_Kim-DevResume2.pdf"
 import qaResume from "IMG/Bell_Kimberly-Resume.pdf"
@@ -22,29 +23,36 @@ const AppBar = () => {
   const [openNav, setOpenNav] = useState(false)
 
   return (
-    <header>
-      <MDBNavbar expand="lg" fixed="top" className="px-3">
-        <MDBContainer fluid>
-          <MDBNavbarBrand href="/" className="pl-2">
-            <img src={Whiskers} alt="Cat whiskers" />
-            <span className="pl-3 fs-4">kimbell.me</span>
-          </MDBNavbarBrand>
-          <MDBNavbarToggler
-            type="button"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setOpenNav(!openNav)}
-          >
-            <MDBIcon icon="bars" fas />
-          </MDBNavbarToggler>
-          <MDBCollapse navbar open={openNav}>
-            <MDBNavbarNav right fullWidth={false} data-testid="nav-links">
-              <MDBNavbarLink href="#about">About</MDBNavbarLink>
-              <MDBNavbarLink href="#skills">Skills</MDBNavbarLink>
-              <MDBNavbarLink href="#projects">Projects</MDBNavbarLink>
+    <MDBNavbar expand="lg" fixed="top" className="px-3">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="/" className="pl-2">
+          <img src={Whiskers} alt="Cat whiskers" />
+          <span className="pl-3 fs-4">kimbell.me</span>
+        </MDBNavbarBrand>
 
-              {/* NOTE: MDB does not recognize "target" as prop of MDBDropdownItem */}
-              <div data-testid="resume-dropdown">
+        <MDBNavbarToggler
+          type="button"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setOpenNav(!openNav)}
+        >
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar open={openNav} className="flex-grow-0">
+          <MDBNavbarNav fullWidth={false} data-testid="nav-links">
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#about">About</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#skills">Skills</MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink href="#projects">Projects</MDBNavbarLink>
+            </MDBNavbarItem>
+
+            {/* NOTE: MDB does not recognize "target" as prop of MDBDropdownItem */}
+            <div data-testid="resume-dropdown">
+              <MDBNavbarItem>
                 <MDBDropdown>
                   <MDBDropdownToggle tag="a" className="nav-link">
                     Resumes
@@ -58,12 +66,12 @@ const AppBar = () => {
                     </MDBDropdownItem>
                   </MDBDropdownMenu>
                 </MDBDropdown>
-              </div>
-            </MDBNavbarNav>
-          </MDBCollapse>
-        </MDBContainer>
-      </MDBNavbar>
-    </header>
+              </MDBNavbarItem>
+            </div>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   )
 }
 
